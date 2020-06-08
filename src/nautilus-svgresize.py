cd33d00f89345e1b856ec2a4b5325eb0968e1e10
Lorenzo <lorenzo.carbonell.cerezo@gmail.com>
2020-06-08 20:34:36 +0200

    nanecalib

--- src/nautilus-svgresize.py
+++ src/nautilus-svgresize.py
@@ -3,151 +3,90 @@
 #
 # This file is part of nautilus-iconify
 #
-# Copyright (C) 2016 Lorenzo Carbonell
-# lorenzo.carbonell.cerezo@gmail.com
+# Copyright (c) 2016 Lorenzo Carbonell Cerezo <a.k.a. atareao>
 #
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
 #
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 
 import gi
 try:
     gi.require_version('Gtk', '3.0')
-    gi.require_version('GLib', '2.0')
+    gi.require_version('GObject', '2.0')
     gi.require_version('Nautilus', '3.0')
     gi.require_version('Rsvg', '2.0')
 except Exception as e:
     print(e)
     exit(-1)
 from gi.repository import Gtk
-from gi.repository import GLib
 from gi.repository import GObject
-from gi.repository import Rsvg
 from gi.repository import Nautilus as FileManager
-from PIL import Image
-from urllib import unquote_plus
-import cairo
-import shutil
-from concurrent import futures
+from gi.repository import Rsvg
+import sys
 import os
+import locale
+import gettext
+from PIL import Image
+import cairo
+try:
+    sys.path.insert(1, '/usr/share/nanecalib')
+    from nanecalib import DoItInBackground
+except Exception as nanecalib_error:
+    print(nanecalib_error)
+    sys.exit(-1)
 
-
-APPNAME = '$APP$'
+APP = '$APP$'
 ICON = '$APP$'
 VERSION = '$VERSION$'
+LANGDIR = os.path.join('usr', 'share', 'locale-langpack')
 
-_ = str
-
-
-class Progreso(Gtk.Dialog):
-    __gsignals__ = {
-        'i-want-stop': (GObject.SIGNAL_RUN_FIRST, GObject.TYPE_NONE, ()),
-    }
-
-    def __init__(self, title, parent):
-        Gtk.Dialog.__init__(self, title, parent,
-                            Gtk.DialogFlags.MODAL |
-                            Gtk.DialogFlags.DESTROY_WITH_PARENT)
-        self.set_position(Gtk.WindowPosition.CENTER_ALWAYS)
-        self.set_size_request(330, 30)
-        self.set_resizable(False)
-        self.connect('destroy', self.close)
-        self.set_modal(True)
-        vbox = Gtk.VBox(spacing=5)
-        vbox.set_border_width(5)
-        self.get_content_area().add(vbox)
-        #
-        frame1 = Gtk.Frame()
-        vbox.pack_start(frame1, True, True, 0)
-        table = Gtk.Table(2, 2, False)
-        frame1.add(table)
-        #
-        self.label = Gtk.Label()
-        table.attach(self.label, 0, 2, 0, 1,
-                     xpadding=5,
-                     ypadding=5,
-                     xoptions=Gtk.AttachOptions.SHRINK,
-                     yoptions=Gtk.AttachOptions.EXPAND)
-        #
-        self.progressbar = Gtk.ProgressBar()
-        self.progressbar.set_size_request(300, 0)
-        table.attach(self.progressbar, 0, 1, 1, 2,
-                     xpadding=5,
-                     ypadding=5,
-                     xoptions=Gtk.AttachOptions.SHRINK,
-                     yoptions=Gtk.AttachOptions.EXPAND)
-        button_stop = Gtk.Button()
-        button_stop.set_size_request(40, 40)
-        button_stop.set_image(
-            Gtk.Image.new_from_stock(Gtk.STOCK_STOP, Gtk.IconSize.BUTTON))
-        button_stop.connect('clicked', self.on_button_stop_clicked)
-        table.attach(button_stop, 1, 2, 1, 2,
-                     xpadding=5,
-                     ypadding=5,
-                     xoptions=Gtk.AttachOptions.SHRINK)
-        self.stop = False
-        self.current_size = 0.0
-        self.total_size = 0.0
-        self.is_running = False
-
-        self.show_all()
-
-    def emit(self, *args):
-        GLib.idle_add(GObject.GObject.emit, self, *args)
-
-    def set_total_size(self, total_size):
-        self.total_size = float(total_size)
-
-    def get_stop(self):
-        return self.stop
-
-    def on_button_stop_clicked(self, widget):
-        self.stop = True
-        self.emit('i-want-stop')
-
-    def close(self, *args):
-        self.destroy()
-
-    def increase(self, widget=None, x=1.0):
-        self.current_size += float(x)
-        if self.current_size == self.total_size:
-            GLib.idle_add(self.destroy)
-        else:
-            fraction = float(self.current_size) / float(self.total_size)
-            GLib.idle_add(self.progressbar.set_fraction, fraction)
-
-    def set_element(self, widget=None, element=''):
-        GLib.idle_add(self.label.set_text, str(element))
+current_locale, encoding = locale.getdefaultlocale()
+language = gettext.translation(APP, LANGDIR, [current_locale])
+language.install()
+try:
+    _ = language.gettext
+except Exception:
+    _ = str
 
 
-def resize_svg(args):
-    svg_file, svg_file_resized, width, height, png, diib = args
-    diib.emit('start_one', os.path.basename(svg_file))
-    if os.path.exists(svg_file):
+class ConvertDIIB(DoItInBackground):
+    def __init__(self, title, parent, files, width, height, png):
+        self._width = width
+        self._height = height
+        self._png = png
+        DoItInBackground.__init__(self, title, parent, files, ICON)
+
+    def process_item(self, file_in):
+        width = self._width
+        height = self._height
+        png = self._png
+        head, tail = os.path.split(file_in)
+        root, ext = os.path.splitext(tail)
         if png is True:
-            png_file = os.path.splitext(svg_file_resized)[0] + '.png'
+            png_file = os.path.join(head, root + '.png')
             surface = cairo.ImageSurface(cairo.FORMAT_ARGB32,
                                          int(width),
                                          int(height))
             ctx = cairo.Context(surface)
-            svg = Rsvg.Handle.new_from_file(svg_file)
+            svg = Rsvg.Handle.new_from_file(file_in)
             dimensions = svg.get_dimensions()
             zw = float(width) / float(dimensions.width)
             zh = float(height) / float(dimensions.height)
-            if zw > zh:
-                z = zh
-            else:
-                z = zw
-            print(zw, zh, z)
+            z = zh if zw > zh else zw
             if z <= 0:
                 raise(Exception)
             if width != height:
@@ -165,106 +104,26 @@ def resize_svg(args):
         else:
             width = round(0.8 * width, 0)
             height = round(0.8 * height, 0)
-            fo = open(svg_file_resized, 'w')
-            print('---', width, height, '---')
-            surface = cairo.SVGSurface(fo, width, height)
-            ctx = cairo.Context(surface)
-            svg = Rsvg.Handle.new_from_file(svg_file)
-            dimensions = svg.get_dimensions()
-            zw = float(width) / float(dimensions.width)
-            zh = float(height) / float(dimensions.height)
-            if zw > zh:
-                z = zh
-            else:
-                z = zw
-            print(zw, zh, z)
-            if z <= 0:
-                raise(Exception)
-            if width != height:
-                if width < height:
-                    ctx.translate(0, (float(height)-float(width))/2.0)
-                else:
-                    ctx.translate((float(width)-float(height))/2.0, 0)
-            ctx.scale(z, z)
-            svg.render_cairo(ctx)
-            surface.flush()
-            surface.finish()
-    else:
-        raise(Exception)
-    diib.emit('end_one', 1.0)
-
-
-class DoItInBackground(GObject.GObject):
-    __gsignals__ = {
-        'started': (GObject.SIGNAL_RUN_FIRST, GObject.TYPE_NONE, (int,)),
-        'ended': (GObject.SIGNAL_RUN_FIRST, GObject.TYPE_NONE, (bool,)),
-        'start_one': (GObject.SIGNAL_RUN_FIRST, GObject.TYPE_NONE, (str,)),
-        'end_one': (GObject.SIGNAL_RUN_FIRST, GObject.TYPE_NONE, (float,)),
-    }
-
-    def __init__(self, title, parent, files, options):
-        GObject.GObject.__init__(self)
-        self.files = files
-        self.options = options
-        self.stopit = False
-        self.ok = True
-        self.progreso = Progreso(title, parent)
-        self.progreso.set_total_size(len(files))
-        self.connect('start_one', self.progreso.set_element)
-        self.connect('end_one', self.progreso.increase)
-        self.connect('ended', self.progreso.close)
-
-    def emit(self, *args):
-        GLib.idle_add(GObject.GObject.emit, self, *args)
-
-    def stop(self, *args):
-        self.stopit = True
-
-    def run(self):
-        try:
-            width = self.options['width']
-            height = self.options['height']
-            png = self.options['png']
-            dirname = os.path.join(os.path.dirname(self.files[0]),
-                                   '{0}x{1}'.format(width, height))
-            create_directory(dirname)
-            executor = futures.ThreadPoolExecutor(max_workers=4)
-            for svg_file in self.files:
-                pafile = os.path.basename(svg_file)
-                print(pafile)
-                executor.submit(resize_svg, (
-                    svg_file, os.path.join(dirname,
-                                           os.path.basename(svg_file)), width,
-                    height, png, self))
-            self.progreso.run()
-        except Exception as e:
-            self.ok = False
-            print(e)
-        self.emit('ended', self.ok)
-
-
-def create_directory(dirname):
-    if not os.path.exists(dirname):
-        os.makedirs(dirname)
-
-
-def remove_directory(dirname):
-    if os.path.exists(dirname):
-        shutil.rmtree(dirname)
-
-
-def get_duration(file_in):
-    return os.path.getsize(file_in)
-
-
-def get_files(files_in):
-    files = []
-    for file_in in files_in:
-        print(file_in)
-        file_in = unquote_plus(file_in.get_uri()[7:])
-        if os.path.isfile(file_in):
-            files.append(file_in)
-    return files
+            svg_file_resized = os.path.join(head, root + '_resized.svg')
+            with open(svg_file_resized, 'w') as fo:
+                surface = cairo.SVGSurface(fo, width, height)
+                ctx = cairo.Context(surface)
+                svg = Rsvg.Handle.new_from_file(file_in)
+                dimensions = svg.get_dimensions()
+                zw = float(width) / float(dimensions.width)
+                zh = float(height) / float(dimensions.height)
+                z = zh if zw > zh else zw
+                if z <= 0:
+                    raise(Exception)
+                if width != height:
+                    if width < height:
+                        ctx.translate(0, (float(height)-float(width))/2.0)
+                    else:
+                        ctx.translate((float(width)-float(height))/2.0, 0)
+                ctx.scale(z, z)
+                svg.render_cairo(ctx)
+                surface.flush()
+                surface.finish()
 
 
 class ResizeDialog(Gtk.Dialog):
@@ -321,27 +180,17 @@ class ResizeDialog(Gtk.Dialog):
 class SVGResizeMenuProvider(GObject.GObject, FileManager.MenuProvider):
 
     def __init__(self):
-        pass
+        GObject.GObject.__init__(self)
 
-    def all_files_are_svg(self, items):
-        for item in items:
-            fileName, fileExtension = os.path.splitext(unquote_plus(
-                item.get_uri()[7:]))
-            if fileExtension.lower() != '.svg':
-                return False
-        return True
-
-    def resize(self, menu, selected, window):
+    def process(self, menu, files, window):
         rd = ResizeDialog(window)
         if rd.run() == Gtk.ResponseType.ACCEPT:
             rd.hide()
-            options = {}
-            options['width'] = int(rd.options['width'].get_text())
-            options['height'] = int(rd.options['height'].get_text())
-            options['png'] = rd.options['png'].get_active()
-            files = get_files(selected)
-            diib = DoItInBackground(_('Resize svg images'), window, files,
-                                    options)
+            width = int(rd.options['width'].get_text())
+            height = int(rd.options['height'].get_text())
+            png = rd.options['png'].get_active()
+            diib = ConvertDIIB(_('Resize svg images'), window, files, width,
+                               height, png)
             diib.run()
         rd.destroy()
 
@@ -351,7 +200,14 @@ class SVGResizeMenuProvider(GObject.GObject, FileManager.MenuProvider):
         right-click menu, connects its 'activate' signal to the 'run'\
         method passing the selected Directory/File
         """
-        if self.all_files_are_svg(sel_items):
+        files = []
+        for file_in in sel_items:
+            if not file_in.is_directory():
+                afile = file_in.get_location().get_path()
+                filename, fileextension = os.path.splitext(afile)
+                if fileextension.lower() == '.svg':
+                    files.append(afile)
+        if files:
             top_menuitem = FileManager.MenuItem(
                 name='SVGResizeMenuProvider::Gtk-svgresize-top',
                 label=_('Resize svg files...'),
@@ -365,7 +221,7 @@ class SVGResizeMenuProvider(GObject.GObject, FileManager.MenuProvider):
                 tip=_('Resize svg files'))
             sub_menuitem_00.connect('activate',
                                     self.resize,
-                                    sel_items,
+                                    files,
                                     window)
             submenu.append_item(sub_menuitem_00)
             sub_menuitem_01 = FileManager.MenuItem(
@@ -374,46 +230,42 @@ class SVGResizeMenuProvider(GObject.GObject, FileManager.MenuProvider):
                 tip=_('About'))
             sub_menuitem_01.connect('activate', self.about, window)
             submenu.append_item(sub_menuitem_01)
-            #
+
             return top_menuitem,
         return
 
     def about(self, widget, window):
         ad = Gtk.AboutDialog(parent=window)
-        ad.set_name(APPNAME)
+        ad.set_name(APP)
         ad.set_version(VERSION)
         ad.set_copyright('Copyrignt (c) 2017\nLorenzo Carbonell')
-        ad.set_comments(APPNAME)
+        ad.set_comments(APP)
         ad.set_license('''
-This program is free software: you can redistribute it and/or modify it under
-the terms of the GNU General Public License as published by the Free Software
-Foundation, either version 3 of the License, or (at your option) any later
-version.
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
 
-This program is distributed in the hope that it will be useful, but WITHOUT
-ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
 
-You should have received a copy of the GNU General Public License along with
-this program. If not, see <http://www.gnu.org/licenses/>.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
 ''')
-        ad.set_website('http://www.atareao.es')
-        ad.set_website_label('http://www.atareao.es')
+        ad.set_website('https://www.atareao.es')
+        ad.set_website_label('atareao.es')
         ad.set_authors([
-            'Lorenzo Carbonell <lorenzo.carbonell.cerezo@gmail.com>'])
+            'Lorenzo Carbonell <a.k.a. atareao>'])
         ad.set_documenters([
-            'Lorenzo Carbonell <lorenzo.carbonell.cerezo@gmail.com>'])
+            'Lorenzo Carbonell <a.k.a. atareao>'])
         ad.set_icon_name(ICON)
-        ad.set_logo_icon_name(APPNAME)
+        ad.set_logo_icon_name(APP)
         ad.run()
         ad.destroy()
-
-
-if __name__ == '__main__':
-    import glob
-    options = {'width': 256, 'height': 256, 'png': False}
-    files = glob.glob('/home/lorenzo/Escritorio/Trasuntu-iconos_2.0/\
-scalable/apps/*.svg')
-    diib = DoItInBackground('Test', None, files, options)
-    diib.run()
-    exit(0)
